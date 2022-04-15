b5169572497381e376d8553a59bcca04ba9f2d66
Nikita Rehemae <nikita.rehemae@gmail.com>
2022-04-15 19:18:26 +0300

    Install and set up react-router-dom library

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { store } from "./redux/store";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
+import { store } from "./redux/store";
 import App from './App';
 import './index.css';
 
@@ -11,9 +12,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <Provider store={store}>
-          <App />
-      </Provider>
+      <BrowserRouter>
+          <Provider store={store}>
+              <App />
+          </Provider>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
