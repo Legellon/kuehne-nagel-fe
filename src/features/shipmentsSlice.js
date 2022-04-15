de869f960a98ab9cb507057ec352b0e009c45fb6
Nikita Rehemae <nikita.rehemae@gmail.com>
2022-04-15 17:45:40 +0300

    Create a shipments slice and import it to the store

---
+++ src/features/shipmentsSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+const shipmentsSlice = createSlice({
+    name: 'shipments',
+    initialState,
+    reducers: {
+
+    }
+});
+
+export default shipmentsSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import shipmentsReducer from '../features/shipmentsSlice';
 
 export const store = configureStore({
    reducer: {
-
+      shipments: shipmentsReducer,
    }
 });
