4a0a7c3894a5a2425ba4920eb53ab3276de75683
Nikita Rehemäe <nikita.rehemae@gmail.com>
2022-04-18 19:57:36 +0300

    Add shipments-dashboard #3
    * Add a default route
    
    * Remove icons source from HTML
    
    * Move a browser router to the app from index
    
    * Add prefetched data for offline session
    
    * Add react-router library
    
    * Create shipments-dashboard and move all related files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import ShipmentsDashboard from "./pages/shipment-dashboard";
+import {Routes, Route} from "react-router";
+import {BrowserRouter} from "react-router-dom";
+
 function App() {
     return (
-        <>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<ShipmentsDashboard />} />
+                <Route path='*' element={<>Page not found</>} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
 
 import { store } from "./redux/store";
 import App from './App';
@@ -12,11 +11,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
-          <Provider store={store}>
-              <App />
-          </Provider>
-      </BrowserRouter>
+      <Provider store={store}>
+          <App />
+      </Provider>
   </React.StrictMode>
 );
 
